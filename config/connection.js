52add297b1412ee3d9c0cd868ae37876cdb12530
lightoftwelve <info@lightoftwelve.com>
2023-09-14 10:27:27 -0400

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,8 @@ const Sequelize = require('sequelize');
 
 let sequelize;
 
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // For self-signed certificates
-      }
-    }
-  });
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -26,6 +18,30 @@ if (process.env.DATABASE_URL) {
     }
   );
 }
+
+// if (process.env.DATABASE_URL) {
+//   sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'mysql',
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false // For self-signed certificates
+//       }
+//     }
+//   });
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: '127.0.0.1',
+//       dialect: 'mysql',
+//       port: 3306,
+//       logging: false,
+//     }
+//   );
+// }
 
 module.exports = sequelize;
 // if (process.env.JAWSDB_URL) {
