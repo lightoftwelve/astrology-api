48d00714bbd0ead123419738c96808b74ae0dff9
lightoftwelve <info@lightoftwelve.com>
2023-09-14 10:29:08 -0400

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -19,43 +19,4 @@ if (process.env.JAWSDB_URL) {
   );
 }
 
-// if (process.env.DATABASE_URL) {
-//   sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: 'mysql',
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false // For self-signed certificates
-//       }
-//     }
-//   });
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: '127.0.0.1',
-//       dialect: 'mysql',
-//       port: 3306,
-//       logging: false,
-//     }
-//   );
-// }
-
 module.exports = sequelize;
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: '127.0.0.1',
-//       dialect: 'mysql',
-//       port: 3306,
-//       logging: false,
-//     }
-//   );
-// }
