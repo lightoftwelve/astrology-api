54daffe82dc95f0fcc9ee4aa493b9163bfd6b9de
lightoftwelve <info@lightoftwelve.com>
2023-09-14 11:27:43 -0400

    added s3 integration for photo hosting

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,39 @@
 const router = require('express').Router();
-
+const AWS = require('aws-sdk');
 const apiRoutes = require('./api');
 const celestialRoutes = require('./celestialRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
+
+// Initialize the S3 client
+const s3 = new AWS.S3();
+
+router.get('/images/:imageName', async (req, res) => {
+  try {
+    // Extract the image name from the request params
+    const imageName = req.params.imageName;
+
+    // Construct the S3 key based on naming convention
+    const s3Key = `images/${imageName}`;
+
+    // Specifing S3 bucket and S3 key
+    const params = {
+      Bucket: 'lightoftwelve-astrology-app-images',
+      Key: s3Key,
+    };
+
+    // Retrieve the image from S3
+    const data = await s3.getObject(params).promise();
+
+    // Set response headers
+    res.setHeader('Content-Type', data.ContentType);
+
+    // Send the image data as the response
+    res.send(data.Body);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error retrieving the image');
+  }
+});
 
 router.use('/', dashboardRoutes);
 router.use('/api', apiRoutes);
