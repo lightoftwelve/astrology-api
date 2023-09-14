f4e556c35f5e2a78c1da3681b2e1164b6b2e1f81
lightoftwelve <info@lightoftwelve.com>
2023-09-14 14:15:22 -0400

    updated styling on login and header for mobile

--- public/js/header.js
+++ public/js/header.js
@@ -1,37 +1,41 @@
 document.addEventListener('DOMContentLoaded', async function () {
-    const toggleButton = document.querySelector('.toggle');
     const navigationLinks = document.querySelector('.links');
     const loginLinks = document.querySelector('.sign-in');
 
     const signInButton = document.querySelector('#signInButton');
     const signOutButton = document.querySelector('#signOutButton');
 
-    // Function to handle screen size change
+    // Close and open the menu using only vanilla JS
+    document.querySelector('.toggle').addEventListener('click', function () {
+        this.classList.toggle('active');
+        navigationLinks.classList.toggle('open');
+
+        if (navigationLinks.classList.contains('open')) {
+            navigationLinks.style.display = 'flex';
+        } else {
+            navigationLinks.style.display = 'none';
+            loginLinks.style.display = 'none';
+        }
+    });
+
+    // Close the menu by clicking outside or on the close button using vanilla JS
+    navigationLinks.addEventListener('click', function (event) {
+        if (event.target === this || event.target.classList.contains('close-menu')) {
+            navigationLinks.classList.remove('open');
+            navigationLinks.style.display = 'none';
+            document.querySelector('.toggle').classList.remove('active');
+        }
+    });
+
     function handleScreenSize() {
         if (window.innerWidth > 991) {
-            // For screens wider than 991px (desktop), display the links
             navigationLinks.style.display = 'flex';
             loginLinks.style.display = 'flex';
         } else {
-            // For screens 991px or narrower (mobile), hide the links & signin
             navigationLinks.style.display = 'none';
             loginLinks.style.display = 'none';
         }
     }
-
-    // Toggle the menu on button click
-    toggleButton.onclick = function () {
-        this.classList.toggle('active');
-        navigationLinks.classList.toggle('active');
-
-        // Toggle the display property of links
-        if (navigationLinks.classList.contains('active')) {
-            navigationLinks.style.display = 'flex'; // Display links when active
-        } else {
-            navigationLinks.style.display = 'none'; // Hide links when not active
-            loginLinks.style.display = 'none'; // Hide login icon when not active
-        }
-    };
 
     const checkLoggedIn = async () => {
         try {
