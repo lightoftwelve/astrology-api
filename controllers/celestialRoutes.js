202c8bfc4ca20c3721f9e89160172f3bacf01d00
lightoftwelve <info@lightoftwelve.com>
2023-09-14 12:43:10 -0400

    updated birthchart route to ensure proper access

--- controllers/celestialRoutes.js
+++ controllers/celestialRoutes.js
@@ -34,7 +34,7 @@ router.get('/generate-personalized-astrology-natal-chart', isAuthenticatedView, 
     console.log('celestial Body instance', celestialBodies);
 
     // Fetch astrology aspect data
-    const validAstrologyAspects = await AspectDescriptionData.findAll({
+    const validAstrologyAspects = await AstrologyAspectData.findAll({
       where: { user_id: userId }
     });
 
